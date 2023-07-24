09a85c9470a399c7fe93a0aafe39cfe77750b3e3
Daam Ricardi <damianricardi96@gmail.com>
2023-07-24 20:29:20 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 let carrito = [];
 let totalCarrito = 0;
 
+if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    totalCarrito = parseFloat(localStorage.getItem("totalCarrito")) || 0;
+}
+
 function verCarrito() {
     let carritoContenido = document.getElementById("carrito-lista").innerHTML;
     alert("Contenido del carrito:\n\n" + carritoContenido);
@@ -23,6 +28,7 @@ function agregarAlCarrito(producto, precio, imagen) {
     console.log("Producto agregado al carrito: " + producto);
     console.log("Contenido del carrito: " + JSON.stringify(carrito));
     actualizarCarrito();
+    guardarEnLocalStorage();
     Swal.fire({
         position: 'center',
         icon: 'success',
@@ -32,7 +38,86 @@ function agregarAlCarrito(producto, precio, imagen) {
     })
 }
 
-function finalizarPedido() {
+function removerDelCarrito(index) {
+    let productoRemovido = carrito[index];
+    totalCarrito -= productoRemovido.precio;
+    carrito.splice(index, 1);
+    console.log("Producto removido del carrito en el índice: " + index);
+    console.log("Contenido del carrito actualizado: " + JSON.stringify(carrito));
+    actualizarCarrito();
+    guardarEnLocalStorage();
+}
+
+
+function realizarPedido(){
+    document.body.innerHTML = 
+    ` 
+    <header>
+        <div>
+        <h1><a class="titulo" href="../index.html">ArtMundo3D</a></h1>
+        </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href="index.html">Inicio</a></li>
+                        <li><a href="pages/categoria.html">Filamento</a></li>
+                        <li><a href="pages/acercade.html">Acerca De</a></li>
+                        <li><a href="pages/contacto.html">Contacto</a></li>
+                    </ul>
+                </nav>
+                <button id="ver-carrito" onclick="mostrarCarrito()">Ver Carrito (<span id="carrito-cantidad">0</span>)</button>
+            </div>
+    </header>
+
+    <section id="formulario-pedido">
+    <h2 style="text-align:center; margin-top:20px">Realizar Pedido</h2>
+    <div id="formulario-container">
+    <form onsubmit="finalizarPedidoFormulario(event)">
+        <label for="nombre">Nombre:</label>
+        <input type="text" id="nombre" required><br>
+        <label for="numero-tarjeta">Número de Tarjeta:</label>
+        <input type="text" id="numero-tarjeta" required><br>
+        <label for="fecha-vencimiento">Fecha de Vencimiento:</label>
+        <input type="text" id="fecha-vencimiento" required><br>
+        <label for="cvv">CVV:</label>
+        <input type="text" id="cvv" required><br>
+        <button onclick="finalizarPedi()">Finalizar Pedido</button>
+    </form>
+    </div>
+</section>
+
+    <footer>
+            <div>
+                <h2><a class="titulo" href="index.html">ArtMundo3D</a></h2>
+            </div>
+            <div>
+                <p>Proyecto Curso Coderhouse</p>
+            </div>
+            <div>
+                <p>Proyecto Realizado por:</p>
+                <p>Damian Ricardi®</p>
+            </div>
+        </footer>`;
+}
+function finalizarPedi() {
+    let carritoContainer = document.getElementById("carrito-container");
+    let pedidoExitoContainer = document.getElementById("pedido-exito");
+    carritoContainer.classList.add("hidden");
+    pedidoExitoContainer.classList.remove("hidden");
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'PEDIDO FINALIZADO',
+        showConfirmButton: false,
+        timer: 2500
+    }).then(function () {
+        carrito = [];
+        totalCarrito = 0;
+        guardarEnLocalStorage();
+    })
+}
+
+function finalizarPedidos() {
     let carritoContainer = document.getElementById("carrito-container");
     let pedidoExitoContainer = document.getElementById("pedido-exito");
     carritoContainer.classList.add("hidden");
@@ -46,19 +131,6 @@ function finalizarPedido() {
     })
     carrito = [];
     totalCarrito = 0;
-    actualizarCarrito();
-}
-
-function verCarrito() {
-    console.log("Contenido del carrito: " + carrito);
-}
-
-function removerDelCarrito(index) {
-    let productoRemovido = carrito[index];
-    totalCarrito -= productoRemovido.precio;
-    carrito.splice(index, 1);
-    console.log("Producto removido del carrito: " + index);
-    console.log("Carrito actualizado: " + JSON.stringify(carrito));
     actualizarCarrito();
 }
 
@@ -82,12 +154,16 @@ function actualizarCarrito() {
     let botonRemover = document.createElement("button");
     botonRemover.textContent = "Remover";
     botonRemover.onclick = (function(index) {
-        return function() {
-        removerDelCarrito(index);
-        };
+    return function() {
+    removerDelCarrito(index);
+    };
     })(i);
-    li.appendChild(botonRemover);
-
-    carritoLista.appendChild(li);
+li.appendChild(botonRemover);
+carritoLista.appendChild(li);
 }
+}
+
+function guardarEnLocalStorage() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    localStorage.setItem("totalCarrito", totalCarrito.toString());
 }
